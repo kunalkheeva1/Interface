0591e145a7163ca8aafd06c129640b103728f0e1
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-04-03 21:04:02 -0700

    initial

--- src/Interface.java
+++ src/Interface.java
@@ -15,7 +15,35 @@ class Car implements Vehicle{
     public void applyBrakes(int decrement){
         speed= speed - decrement;
     }
+    public void print(){
+        System.out.println("Gear: "+gear+ "\nSpeed: "+ speed );
+    }
+}
+class Bike implements Vehicle{
+    int gear;
+    int speed;
+    public void changeGear(int newGear){
+        gear= newGear;
+    }
+    public void speedUp(int increment){
+        speed += increment;
+    }
+    public void applyBrakes(int decrement){
+        speed= speed - decrement;
+    }
+    public void print(){
+        System.out.println("Gear: "+gear+ "\nSpeed: "+ speed );
+    }
 }
 
+
 public class Interface {
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.changeGear(2);
+        car.speedUp(254324654);
+        car.applyBrakes(4534534);
+        car.print();
+    }
+
 }
